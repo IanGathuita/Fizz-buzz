2adc3498292bba43bd8441a9d6e79468358d0392
IanJoseph <Ianjoseph630@gmail.com>
2022-01-25 13:12:59 +0300

    First attempt of fizz buzz chalenge in JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+let max = 100;
+for(let number = 1;number <= max; number++){
+    if(number%3 === 0 && number%5 === 0){
+        console.log("FizzBuzz");
+    }
+    else if(number%3 === 0){
+        console.log("Fizz");
+    }
+    else if(number%5 === 0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(number);
+    }
+}
+
+
+
